6cfc504d4c7ce8e7a05ab8487c3a56619697e0d8
Sylvain DEKER <sylvain.deker.etd@gmx.com>
2018-05-03 15:23:10 +0200

    LightCreator.ui completed

--- Applications/MainApplication_BE/Gui/LightCreator.cpp
+++ Applications/MainApplication_BE/Gui/LightCreator.cpp
@@ -1,13 +1,33 @@
 #include <Gui/LightCreator.hpp>
-
+#include <QPushButton>
+#include <QColorDialog>
+#include <QPalette>
 
 
 namespace Ra {
 namespace Gui {
 LightCreator::LightCreator( QWidget* parent ) : QWidget( parent )    {
     setupUi( this );
+    m_color = QColor(255,255,255);
+    QPalette p;
+
+    p.setColor(QPalette::Background,m_color);
+    m_result_color->setAutoFillBackground(true);
+    m_result_color->setPalette(p);
+    connect(m_button_color,&QPushButton::clicked,this,&LightCreator::open_dialogColor);
 
 }
+
+
+void LightCreator::open_dialogColor(){
+  m_color = QColorDialog::getColor ();
+  QPalette p;
+
+  p.setColor(QPalette::Background,m_color);
+  m_result_color->setPalette(p);
+
+}
+
 
 
 } // namespace Gui
