48e46369ac38cfa15c43743fcf8b61387b3ef190
Sylvain DEKER <sylvain.deker.etd@gmx.com>
2018-05-03 16:48:13 +0200

    Gui Light Creator : Select color is made

--- Applications/MainApplication_BE/Gui/LightCreator.cpp
+++ Applications/MainApplication_BE/Gui/LightCreator.cpp
@@ -1,13 +1,33 @@
 #include <Gui/LightCreator.hpp>
-
+#include <QPushButton>
+#include <QColorDialog>
+#include <QPalette>
 
 
 namespace Ra {
 namespace Gui {
 LightCreator::LightCreator( QWidget* parent ) : QWidget( parent )    {
     setupUi( this );
+    color = QColor(255,255,255);
+    QPalette p;
+
+    p.setColor(QPalette::Background,color);
+    result_color->setAutoFillBackground(true);
+    result_color->setPalette(p);
+    connect(button_color,&QPushButton::clicked,this,&LightCreator::open_dialogColor);
 
 }
+
+
+void LightCreator::open_dialogColor(){
+  color = QColorDialog::getColor ();
+  QPalette p;
+
+  p.setColor(QPalette::Background,color);
+  result_color->setPalette(p);
+
+}
+
 
 
 } // namespace Gui
