3fcd4310d60363a5e33fbedf00fcecc66742d6ad
Sylvain DEKER <sylvain.deker.etd@gmx.com>
2018-05-04 15:20:28 +0200

    Gui LightCreator basic connect

--- Applications/MainApplication_BE/Gui/LightCreator.cpp
+++ Applications/MainApplication_BE/Gui/LightCreator.cpp
@@ -2,7 +2,10 @@
 #include <QPushButton>
 #include <QColorDialog>
 #include <QPalette>
-
+#include <QDoubleSpinBox>
+#include <QSlider>
+#include <QAbstractSpinBox>
+#include <math.h>
 
 namespace Ra {
 namespace Gui {
@@ -14,10 +17,33 @@ LightCreator::LightCreator( QWidget* parent ) : QWidget( parent )    {
     p.setColor(QPalette::Background,m_color);
     m_result_color->setAutoFillBackground(true);
     m_result_color->setPalette(p);
-    connect(m_button_color,&QPushButton::clicked,this,&LightCreator::open_dialogColor);
+    connect(m_button_color, &QPushButton::clicked,this,&LightCreator::open_dialogColor);
+
+    // Intensity
+    connect(m_intensity_slider, &QSlider::valueChanged, this , &LightCreator::slot_intensity_slide_to_spin );
+    connect(this,&LightCreator::sig_intensity_slide_to_spin,m_intensity_spinbox,&QDoubleSpinBox::setValue);
+
+    connect(m_intensity_spinbox, static_cast<void (QDoubleSpinBox::*) (double)>(&QDoubleSpinBox::valueChanged), this , &LightCreator::slot_intensity_spin_to_slide );
+    connect(this,&LightCreator::sig_intensity_spin_to_slide,m_intensity_slider,&QSlider::setValue);
+
+    // Angle
+    connect(m_angle_slider, &QSlider::valueChanged, this , &LightCreator::slot_angle_slide_to_spin );
+    connect(this,&LightCreator::sig_angle_slide_to_spin,m_angle_spinbox,&QDoubleSpinBox::setValue);
+
+    connect(m_angle_spinbox, static_cast<void (QDoubleSpinBox::*) (double)>(&QDoubleSpinBox::valueChanged), this , &LightCreator::slot_angle_spin_to_slide );
+    connect(this,&LightCreator::sig_angle_spin_to_slide,m_angle_slider,&QSlider::setValue);
+
+    // Falloff
+
+    connect(m_falloff_slider, &QSlider::valueChanged, this , &LightCreator::slot_falloff_slide_to_spin );
+    connect(this,&LightCreator::sig_falloff_slide_to_spin,m_falloff_spinbox,&QDoubleSpinBox::setValue);
+
+    connect(m_falloff_spinbox, static_cast<void (QDoubleSpinBox::*) (double)>(&QDoubleSpinBox::valueChanged), this , &LightCreator::slot_falloff_spin_to_slide );
+    connect(this,&LightCreator::sig_falloff_spin_to_slide,m_falloff_slider,&QSlider::setValue);
+
+
 
 }
-
 
 void LightCreator::open_dialogColor(){
   m_color = QColorDialog::getColor ();
@@ -27,6 +53,46 @@ void LightCreator::open_dialogColor(){
   m_result_color->setPalette(p);
 
 }
+
+// Intensity
+void LightCreator::slot_intensity_slide_to_spin(int val){
+  m_intensity_val = (double) val;
+  emit sig_intensity_slide_to_spin(m_intensity_val);
+}
+
+void LightCreator::slot_intensity_spin_to_slide(double val){
+  m_intensity_val = val;
+  int tmp = round(val);
+  emit sig_intensity_spin_to_slide(tmp);
+}
+
+// Angle
+
+void LightCreator::slot_angle_slide_to_spin(int val){
+  m_angle_val = (double) val;
+  emit sig_angle_slide_to_spin(m_angle_val);
+}
+
+void LightCreator::slot_angle_spin_to_slide(double val){
+  m_angle_val = val;
+  int tmp = round(val);
+  emit sig_angle_spin_to_slide(tmp);
+}
+
+
+// Falloff
+void LightCreator::slot_falloff_slide_to_spin(int val){
+  m_falloff_val = (double) val;
+  emit sig_falloff_slide_to_spin(m_falloff_val);
+}
+
+void LightCreator::slot_falloff_spin_to_slide(double val){
+  m_falloff_val = val;
+  int tmp = round(val);
+  emit sig_falloff_spin_to_slide(tmp);
+}
+
+
 
 
 
--- Applications/MainApplication_BE/Gui/LightCreator.hpp
+++ Applications/MainApplication_BE/Gui/LightCreator.hpp
@@ -34,7 +34,20 @@ class LightCreator : public QWidget, private Ui::LightCreator {
 
   private slots:
     void open_dialogColor();
+    void slot_intensity_slide_to_spin(int val);
+    void slot_intensity_spin_to_slide(double val);
+    void slot_angle_slide_to_spin(int val);
+    void slot_angle_spin_to_slide(double val);
+    void slot_falloff_slide_to_spin(int val);
+    void slot_falloff_spin_to_slide(double val);
 
+  signals:
+    void sig_intensity_slide_to_spin(double);
+    void sig_intensity_spin_to_slide(int);
+    void sig_angle_slide_to_spin(double);
+    void sig_angle_spin_to_slide(int);
+    void sig_falloff_slide_to_spin(double);
+    void sig_falloff_spin_to_slide(int);
 
 
 
@@ -46,6 +59,9 @@ class LightCreator : public QWidget, private Ui::LightCreator {
 
   private:
     QColor m_color;
+    double m_intensity_val;
+    double m_angle_val;
+    double m_falloff_val;
 
 
 };
