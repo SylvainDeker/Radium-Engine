341eae2ae2e1b82bc9744642fd1ef9c75190bc00
Sylvain DEKER <sylvain.deker.etd@gmx.com>
2018-05-24 21:26:17 +0200

    Add Rotate Gizmo to PointLight

--- src/Engine/Renderer/Light/DirLight.cpp
+++ src/Engine/Renderer/Light/DirLight.cpp
@@ -15,6 +15,38 @@ void DirectionalLight::getRenderParameters( RenderParameters& params ) const {
 
     params.addParameter( "light.directional.direction", m_direction );
 }
+void DirectionalLight::setTransform( Core::Container::Index roIdx, const Core::Math::Transform& transform ){
+    (void) roIdx;
+
+    if(transform(0,0)==1 && transform(0,1)==0 && transform(0,2)==0 ){ // X
+      m_direction = Core::Math::Vector3(
+        m_direction.x()  ,
+        (m_direction.y() * transform(1,1) + m_direction.z() * transform(1,2)) ,
+        (m_direction.y() * transform(2,1) + m_direction.z() * transform(2,2))
+      );
+    }
+    else  if (transform(1,0)==0 && transform(1,1)==1 && transform(1,2)==0){ // Y
+      m_direction = Core::Math::Vector3(
+          ( m_direction.x() * transform(0,0) + m_direction.z() * transform(0,2) ),
+          (m_direction.y())  ,
+           (m_direction.x() * transform(2,0) + m_direction.z() * transform(2,2))
+
+      );
+    }
+    else  if (transform(2,0)==0 && transform(2,1)==0 && transform(2,2)==1){ //Z
+      m_direction = Core::Math::Vector3(
+        ( m_direction.x() * transform(0,0) + m_direction.y() * transform(0,1)) ,
+        (m_direction.x() * transform(1,0) + m_direction.y() * transform(1,1))  ,
+        m_direction.z()
+
+      );
+    }
+}
+
+Ra::Core::Math::Transform DirectionalLight::getTransform( Core::Container::Index roIdx ) {
+    (void) roIdx;
+    return Core::Math::Transform::Identity();
+};
 
 std::string DirectionalLight::getShaderInclude() const {
     return "Directional";
