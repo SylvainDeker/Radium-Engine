19e3927386ba0bc538c864a0fe3318d44f89a941
Sylvain DEKER <sylvain.deker.etd@gmx.com>
2018-05-26 15:37:44 +0200

    Add keyword override

--- src/Engine/Renderer/Light/DirLight.hpp
+++ src/Engine/Renderer/Light/DirLight.hpp
@@ -27,13 +27,14 @@ class RA_ENGINE_API DirectionalLight final : public Light {
        \param const Core::Math::Transform& transform the transformation
        \return void
     */
-    void setTransform( Core::Container::Index roIdx, const Core::Math::Transform& transform );
+
+    void setTransform( Core::Container::Index roIdx, const Core::Math::Transform& transform ) override;
 
     /*!
        \brief Redefinition from Component to update Gizmos position when you use them on light
        \return void
     */
-    Ra::Core::Math::Transform getTransform( Core::Container::Index roIdx ) const ;
+    Ra::Core::Math::Transform getTransform( Core::Container::Index roIdx ) const override;
 
   private:
     Core::Math::Vector3 m_direction;
